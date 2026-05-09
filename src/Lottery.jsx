e957fdc1cf4cde92f28face3dbff3e908ba86e0d
Vivek-Patel162 <vivekpatel9646@gmail.com>
2026-05-09 15:26:31 +0530

    some change

--- src/Lottery.jsx
+++ src/Lottery.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import "./Lottery.css";
+
 export default function Lottery(){
+    let [tickect,setTicket]=useState[0,0,0];
     return (
         <div>
             <h1>Lottery</h1>
+            <div className="ticket">
+            <span>{ticket[0]}</span>
+            <span>{ticket[1]}</span>
+            <span>{ticket[2]}</span>
+            </div>
         </div>
     );
 }
